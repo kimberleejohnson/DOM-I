85e7808cba752b187a483a6e3554c1478c35018e
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-08 13:13:54 -0700

    Added in nav bar styling

--- js/index.js
+++ js/index.js
@@ -53,23 +53,28 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 // Adding nav bar 
 
 // Adding a variable so I can select and assign multiple nav items 
-const navItems = document.querySelectorAll('nav a');
-
+const navItems = document.querySelectorAll('nav a'); 
+ 
 const navitem1 = navItems[0]; 
-navitem1.setAttribute('src', siteContent["nav"]["nav-item-1"]);  
+navitem1.setAttribute('src', siteContent["nav"]["nav-item-1"]);
+navitem1.innerText = siteContent["nav"]["nav-item-1"]; 
 
-const navitem2 = navItems[1]; 
-navitem2.setAttribute('src', siteContent["nav"]["nav-item-2"]);  
+ const navitem2 = navItems[1]; 
+ navitem2.setAttribute('src', siteContent["nav"]["nav-item-2"]);
+ navitem2.innerText = siteContent["nav"]["nav-item-2"];   
 
-const navitem3 = navItems[2]; 
-navitem3.setAttribute('src', siteContent["nav"]["nav-item-3"]); 
+ const navitem3 = navItems[2]; 
+ navitem3.setAttribute('src', siteContent["nav"]["nav-item-3"]); 
+ navitem3.innerText = siteContent["nav"]["nav-item-3"]; 
 
-const navitem4 = navItems[3]; 
-navitem4.setAttribute('src', siteContent["nav"]["nav-item-4"]); 
+ const navitem4 = navItems[3]; 
+ navitem4.setAttribute('src', siteContent["nav"]["nav-item-4"]); 
+ navitem4.innerText = siteContent["nav"]["nav-item-4"]; 
 
-const navitem5 = navItems[4]; 
-navitem5.setAttribute('src', siteContent["nav"]["nav-item-5"]); 
+ const navitem5 = navItems[4]; 
+ navitem5.setAttribute('src', siteContent["nav"]["nav-item-5"]); 
+ navitem5.innerText = siteContent["nav"]["nav-item-5"]; 
 
-const navitem6 = navItems[5]; 
-navitem6.setAttribute('src', siteContent["nav"]["nav-item-6"]); 
-console.log(navitem6); 
+ const navitem6 = navItems[5]; 
+ navitem6.setAttribute('src', siteContent["nav"]["nav-item-6"]);
+ navitem6.innerText = siteContent["nav"]["nav-item-6"];  
